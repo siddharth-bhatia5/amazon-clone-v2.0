99f88ebb83c3fefd587c13fb8cd084507b5b5072
sid_the_coder <siddharth.bhatia555@gmail.com>
2021-08-03 18:59:39 -0400

    Add Amazon Clone v2.0 files to GitHub.

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAry-xdEoQd6lAt0NLiS1fLFLMs50uwRmg",
+  authDomain: "v2-b540d.firebaseapp.com",
+  projectId: "v2-b540d",
+  storageBucket: "v2-b540d.appspot.com",
+  messagingSenderId: "1076421503966",
+  appId: "1:1076421503966:web:dd659ab50acbbfa1834b4f",
+  measurementId: "G-VGC7T4DVVL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+
